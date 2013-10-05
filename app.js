af4f2e034d26eeb7925836b3b525e7400f0b7d16
Jonathan Ozeran <jonathan@jonathanozeran.com>
2013-10-05 16:36:54 -0500

    Math game now works, shows winner/loser status accordingly

--- app.js
+++ app.js
@@ -15,31 +15,61 @@ function Game () {
 	var self = this;
 	var int1 = Math.floor(Math.random() * 30) + 1;
 	var int2 = Math.floor(Math.random() * 30) + 1;
+	self.question = int1 + "+" + int2;
+	self.evaluated = eval (self.question);
 }
+
+// Instantiate new game object which holds the two integers, the composed question and the calculated, expected result)
+var game = new Game();
 
 io.sockets.on('connection', function(socket) {
 
 	socket.on('new player', function(data, callback) {
 		// TODO: should probably extend with binary search instead
+		
+		// Check for player name in array		
 		if (playernames.indexOf(data) != -1) {
 			callback(false);
 		}
+		// If not found, handle new player, send list of players and game data
 		else {
 			callback(true);
 			socket.playername = data;
 			playernames.push(socket.playername);
 			io.sockets.emit('playernames', playernames);
+			io.sockets.emit('new game', game.question);
+			console.log(game);
 		}
 	});
 
-
 	socket.on('send answer', function(data) {
+
+		// Use this to fire off the answer submitted to all players inclusive of player name
 		io.sockets.emit('new answer received', {answer: data, playername: socket.playername});
+		
 		// Use to transmit to everyone but the original sender / initiator
 		// socket.broadcast.emit('new answer received', data);
+
+		// Game logic
+		if(data == game.evaluated) {
+			// Let all connected sockets know that there is a winner
+			io.sockets.emit('game over', "winner");
+
+			// Then let the losing sockets know the bad news
+			socket.broadcast.emit('game over', "loser");
+
+			// Now that this game has been decided, start a new one
+			game = new Game();
+
+			// And then send it around to all of the sockets (including the winner)
+			io.sockets.emit('new game', game.question);
+		}
+
 	});
 
 	socket.on('disconnect', function(data) {
+
+		// Look for the player name that disconnected and remove it from the array, then send it around to the open sockets
 		if(!socket.playername)
 			return;
 		playernames.splice(playernames.indexOf(socket.playername), 1);
