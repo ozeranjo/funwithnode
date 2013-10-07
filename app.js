4b85f46884ada6d737ab31876eee7b14dc35eaed
Jonathan Ozeran <jonathan@jonathanozeran.com>
2013-10-06 21:41:00 -0500

    Game now has server-driven score handling (though it resets when server is restarted)

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
 	playernames = [];
+	playerscores = {};
+    points = {};
+
 
 server.listen(3000);
 
@@ -11,7 +14,6 @@ app.get('/', function(req,res) {
 });
 
 function Game () {
-	// TODO: use a different approach for getting a reference to the object, this seems inelegant
 	var self = this;
 	var int1 = Math.floor(Math.random() * 30) + 1;
 	var int2 = Math.floor(Math.random() * 30) + 1;
@@ -19,10 +21,38 @@ function Game () {
 	self.evaluated = eval (self.question);
 }
 
-// Instantiate new game object which holds the two integers, the composed question and the calculated, expected result)
+// Instantiate new game object which holds the two integers, the composed question and the calculated, expected result
 var game = new Game();
 
+function getPoints() {
+	var scores = (function() { 
+		
+		// Need a dummy score array to push the discovered values to
+		var scores = [];
+	
+		// Iterate through all elements of the scores array
+		for (var key in playerscores) {
+			if (playerscores.hasOwnProperty(key)) {
+
+				// Take the name and score and add it to the array we'll be returning
+				scores.push({
+					name: key,
+					score: playerscores[key]
+				});
+			};
+		};
+		
+		// Return the scores array to the outer function
+		return scores;
+
+	})();
+
+	// Get this score update back to the caller
+	return scores;
+};
+
 io.sockets.on('connection', function(socket) {
+io.sockets.emit('players', { data: playernames });
 
 	socket.on('new player', function(data, callback) {
 		// TODO: should probably extend with binary search instead
@@ -36,9 +66,13 @@ io.sockets.on('connection', function(socket) {
 			callback(true);
 			socket.playername = data;
 			playernames.push(socket.playername);
-			io.sockets.emit('playernames', playernames);
-			io.sockets.emit('new game', game.question);
-			console.log(game);
+			io.sockets.emit('players', playernames);
+	
+			// Only start the game if there is more than 1 player...
+			if(playernames.length > 1) {
+				io.sockets.emit('new game', game.question);
+				console.log(game);
+			}
 		}
 	});
 
@@ -58,10 +92,21 @@ io.sockets.on('connection', function(socket) {
 			// Then let the losing sockets know the bad news
 			socket.broadcast.emit('game over', "loser");
 
+			// Assign points to winner and ensure that any new player has a score of 0
+			if(!playerscores[socket.playername]) {
+				playerscores[socket.playername] = 0;
+			}
+
+			playerscores[socket.playername]++;
+
+			//  Retrieve all scores and send the score update to all players
+			points = getPoints();
+			io.sockets.emit('score update', points);
+
 			// Now that this game has been decided, start a new one
 			game = new Game();
 
-			// And then send it around to all of the sockets (including the winner)
+			// And then send the new game's question around to all of the sockets (including the winner)
 			io.sockets.emit('new game', game.question);
 		}
 
@@ -73,7 +118,10 @@ io.sockets.on('connection', function(socket) {
 		if(!socket.playername)
 			return;
 		playernames.splice(playernames.indexOf(socket.playername), 1);
-		io.sockets.emit('playernames', playernames);
+		io.sockets.emit('players', playernames);
+
+		// Note: the score for the disconnecting player name will not be removed though it will be purged when the server restarts
+		// TODO: use persistent Mongo or Redis storage
 	});
 
 });
